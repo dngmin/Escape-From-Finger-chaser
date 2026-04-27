c9de0482543f25fd66ab3813820feb61bd2ad51c
dngmin <kimdongmin2248@gmail.com>
2026-04-28 00:07:36 +0900

    feat: add enemy collision detection and refactor code

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,6 +5,32 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
+// function
+float float_square(float x)
+{
+    return x * x;
+}
+
+float getDistance_square(const sf::CircleShape& player, const sf::CircleShape& chaser)
+{
+    return (float_square(player.getPosition().x - chaser.getPosition().x) + float_square(player.getPosition().y - chaser.getPosition().y));
+}
+
+void chasers_initialize(std::vector<sf::CircleShape>& chasers, sf::Vector2u window_size, float rad)
+{
+    std::vector<sf::Vector2f> init_pos = {{0.1, 0.1}, {0.9, 0.1}, {0.1, 0.9}, {0.9, 0.9}};
+    std::vector<std::vector<u_int8_t>> init_color = {{255, 0, 0}, {0, 255, 0}, {0, 255, 255}, {255, 255, 0}};
+    int i = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        init_pos[i].x *= window_size.x;
+        init_pos[i].y *= window_size.y;
+        chasers[i].setRadius(rad);
+        chasers[i].setOrigin({rad, rad});
+        chasers[i].setPosition(init_pos[i]);
+        chasers[i].setFillColor(sf::Color(init_color[i][0], init_color[i][1], init_color[i][2]));
+    }
+}
 
 const int socket_port_num = 5005;
 
@@ -30,21 +56,7 @@ int main()
     player.setOrigin({rad, rad});
     // 敵
     std::vector<sf::CircleShape> chasers(4);
-    {
-        std::vector<sf::Vector2f> init_pos = {{0.1, 0.1}, {0.9, 0.1}, {0.1, 0.9}, {0.9, 0.9}};
-        std::vector<std::vector<u_int8_t>> init_color = {{255, 0, 0}, {0, 255, 0}, {0, 255, 255}, {255, 255, 0}};
-        int i = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            init_pos[i].x *= window_size.x;
-            init_pos[i].y *= window_size.y;
-            chasers[i].setRadius(rad);
-            chasers[i].setOrigin({rad, rad});
-            chasers[i].setPosition(init_pos[i]);
-            chasers[i].setFillColor(sf::Color(init_color[i][0], init_color[i][1], init_color[i][2]));
-        }    
-    }
-
+    chasers_initialize(chasers, window_size, rad);
 
 
     // finger_posiion 初期化
@@ -79,11 +91,19 @@ int main()
         
 
 
-        
+        // 敵への当たり判定
+        for (const auto& chaser : chasers)
+        {
+            if (getDistance_square(player, chaser) < 4*rad*rad)
+            {
+                std::cout << "BoB";
+                return 0;
+            }
+        }
 
 
         
-
+        // 描画
         window.clear(sf::Color::Black);
         player.setPosition({finger_position.x, finger_position.y});
         window.draw(player);
