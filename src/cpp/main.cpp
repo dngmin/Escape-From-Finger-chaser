532dc68558d64a4d438a22c1e92994b01b8923cc
dngmin <kimdongmin2248@gmail.com>
2026-05-02 15:43:24 +0900

    refactor: add chaser struct and calculate chasing with ratio

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,14 +6,20 @@
 #include <SFML/Network.hpp>
 
 // struct
-struct State
+struct player_State
 {
     sf::Vector2f curr_pos;
-    sf::Vector2f prev_pos;
+    sf::Vector2f prev_pos = {-100.f, -100.f};
     sf::Vector2f curr_vel;
     sf::Vector2f prev_vel;
     sf::Vector2f curr_acc;
     sf::Vector2f prev_acc;
+};
+
+struct chaser_State
+{
+    sf::CircleShape chaser_shape;
+    sf::Vector2f position;
 };
 
 // function
@@ -22,51 +28,42 @@ float float_square(float x)
     return x * x;
 }
 
-float getDistance_square(const sf::CircleShape& player, const sf::CircleShape& chaser)
+float getDistance_square(const sf::Vector2f& A, const sf::Vector2f& B, const sf::Vector2u& window_size)
 {
-    return (float_square(player.getPosition().x - chaser.getPosition().x) + float_square(player.getPosition().y - chaser.getPosition().y));
+    return (float_square(window_size.x * (A.x - B.x)) + float_square(window_size.y * (A.y - B.y)));
 }
 
-void chasers_initialize(std::vector<sf::CircleShape>& chasers, sf::Vector2u window_size, float rad)
+void chasers_initialize(std::vector<chaser_State>& chasers, sf::Vector2u window_size, float rad)
 {
     std::vector<sf::Vector2f> init_pos = {{0.1, 0.1}, {0.9, 0.1}, {0.1, 0.9}, {0.9, 0.9}};
-    std::vector<std::vector<u_int8_t>> init_color = {{255, 0, 0}, {0, 255, 0}, {0, 255, 255}, {255, 255, 0}};
-    int i = 0;
+    std::vector<std::vector<uint8_t>> init_color = {{255, 0, 0}, {0, 255, 0}, {0, 255, 255}, {255, 255, 0}};
     for (int i = 0; i < 4; i++)
     {
+        chasers[i].position = init_pos[i];
         init_pos[i].x *= window_size.x;
         init_pos[i].y *= window_size.y;
-        chasers[i].setRadius(rad);
-        chasers[i].setOrigin({rad, rad});
-        chasers[i].setPosition(init_pos[i]);
-        chasers[i].setFillColor(sf::Color(init_color[i][0], init_color[i][1], init_color[i][2]));
+        chasers[i].chaser_shape.setRadius(rad);
+        chasers[i].chaser_shape.setOrigin({rad, rad});
+        chasers[i].chaser_shape.setPosition(init_pos[i]);
+        chasers[i].chaser_shape.setFillColor(sf::Color(init_color[i][0], init_color[i][1], init_color[i][2]));
     }
 }
 
-void chasing(sf::CircleShape& chaser, const sf::Vector2f& target_pos)
+void chasing(chaser_State& chaser, const sf::Vector2f& target_pos, sf::Vector2u window_size, float chasing_speed = 2e-5)
 {
-    float chasing_speed = 2e-2;
-    sf::Vector2f chaser_pos = chaser.getPosition();
-    float v = sqrt(pow(target_pos.x - chaser_pos.x,2) + pow(target_pos.y - chaser_pos.y,2));
-    float vx = (target_pos.x - chaser_pos.x)/v * chasing_speed;
-    float vy = (target_pos.y - chaser_pos.y)/v * chasing_speed;
-    chaser.setPosition({chaser_pos.x + vx, chaser_pos.y + vy});
+    float v = sqrt(pow(target_pos.x - chaser.position.x,2) + pow(target_pos.y - chaser.position.y,2));
+    chaser.position.x += (target_pos.x - chaser.position.x)/v * chasing_speed;
+    chaser.position.y += (target_pos.y - chaser.position.y)/v * chasing_speed;
+    chaser.chaser_shape.setPosition({window_size.x * (chaser.position.x), window_size.y * (chaser.position.y)});
 }
 
-sf::Vector2f chasing(sf::Vector2f chaser_pos, sf::Vector2f target_pos)
-{
-    float chasing_speed = 2e-2;
-    float v = sqrt(pow(target_pos.x - chaser_pos.x,2) + pow(target_pos.y - chaser_pos.y,2));
-    float vx = (target_pos.x - chaser_pos.x)/v * chasing_speed;
-    float vy = (target_pos.y - chaser_pos.y)/v * chasing_speed;
-    return {vx, vy};
-}
-
-sf::Vector2f predict_Euler(State& state)
+sf::Vector2f predict_Euler(player_State& state)
 {
     // x_t+1 = xt + vt*t + 0.5*a*t^2
+    return {0.f, 0.f};
 }
 
+// constant
 const int socket_port_num = 5005;
 
 int main()
@@ -84,19 +81,19 @@ int main()
     sf::Vector2u window_size = {800, 600};
     sf::RenderWindow window(sf::VideoMode(window_size), "display");
 
-    float rad = 10.f;
+    float player_size_rad = 10.f;
+    float chaser_size_rad = 10.f;
 
     // player
-    sf::CircleShape player(rad);
-    player.setOrigin({rad, rad});
+    sf::CircleShape player(player_size_rad);
+    player.setOrigin({player_size_rad, player_size_rad});
     // 敵
-    std::vector<sf::CircleShape> chasers(4);
-    chasers_initialize(chasers, window_size, rad);
+    std::vector<chaser_State> chasers(4);
+    chasers_initialize(chasers, window_size, chaser_size_rad);
 
 
-    // finger_posiion 初期化
-    State finger;
-    finger.prev_pos = {-100.f, -100.f};
+    // player position 初期化
+    player_State player_state;
 
 
     while (window.isOpen())
@@ -116,16 +113,15 @@ int main()
         
         
         // Socket通信
-        if (socket.receive(&finger.curr_pos, sizeof(finger.curr_pos), received, sender, port) == sf::Socket::Status::Done)
+        if (socket.receive(&player_state.curr_pos, sizeof(player_state.curr_pos), received, sender, port) == sf::Socket::Status::Done)
         {
-            finger.curr_pos.x *= window_size.x;
-            finger.curr_pos.y *= window_size.y;
+            player.setPosition({player_state.curr_pos.x * window_size.x, player_state.curr_pos.y * window_size.y});
         }
 
         // 敵の追跡アルゴリズム
         
         // 単純追跡
-        for (auto& chaser : chasers) chasing(chaser, finger.curr_pos);
+        for (auto& chaser : chasers) chasing(chaser, player_state.curr_pos, window_size);
 
 
         
@@ -134,7 +130,7 @@ int main()
         // 敵への当たり判定
         for (const auto& chaser : chasers)
         {
-            if (getDistance_square(player, chaser) < 4*rad*rad)
+            if (getDistance_square(player_state.curr_pos, chaser.position, window_size) < ((player_size_rad + chaser_size_rad) * (player_size_rad + chaser_size_rad)))
             {
                 return 0;
             }
@@ -144,9 +140,8 @@ int main()
         
         // 描画
         window.clear(sf::Color::Black);
-        player.setPosition({finger.curr_pos.x, finger.curr_pos.y});
         window.draw(player);
-        for (auto& chaser : chasers) window.draw(chaser);
+        for (auto& chaser : chasers) window.draw(chaser.chaser_shape);
 
         window.display();
     }
