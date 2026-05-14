5dd3cb0e13b84cd96fef4117d52143283053af01
dngmin <kimdongmin2248@gmail.com>
2026-05-14 15:04:35 +0900

    feat: add 1€ Filter for jitter

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,21 +1,94 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 // SFML
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
-// struct
-struct player_State
+// constant
+const int socket_port_num = 5005;
+
+// class
+class playerState
 {
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = std::chrono::time_point<Clock>;
+
+    private:
     sf::Vector2f curr_pos;
-    sf::Vector2f prev_pos = {-100.f, -100.f};
+    sf::Vector2f prev_pos;
     sf::Vector2f curr_vel;
     sf::Vector2f prev_vel;
     sf::Vector2f curr_acc;
-    sf::Vector2f prev_acc;
+
+    // 1€ filter 初期値
+    TimePoint T_prev = Clock::now();
+    double dT;
+    static constexpr double min_cutoff_frequency = 1.0; // f_c_min：最小遮断周波数
+    static constexpr double beta = 0.01;
+    static constexpr double derivative_cutoff = 1.0; // d_cutoff；微分値の遮断周波数
+    static constexpr double two_pi = 6.2831853071795865;
+
+    double get_alpha(double dT, double cutoff)
+    {
+        return 1 / (1 + 1 / (two_pi * dT * cutoff));
+    }
+
+    double solve_low_pass_filter(double curr, double prev, double alpha)
+    {
+        return alpha * curr + (1 - alpha) * prev;
+    }
+
+    public:
+    sf::Vector2f get_curr_pos()
+    {
+        return curr_pos;
+    }
+
+    sf::Vector2f get_Euler_predict()
+    {
+        // x_t+1 = xt + vt*dt
+        float predict_x = curr_pos.x + curr_vel.x * (5 * dT);
+        float predict_y = curr_pos.y + curr_vel.y * (5 * dT);
+        return {predict_x, predict_y};
+    }
+
+    void update(sf::Vector2f received_pos)
+    {
+        if (received_pos == curr_pos) return;
+
+        // 1€ filter
+        // calculate velocity
+        TimePoint T = Clock::now();
+        dT = std::chrono::duration<double>(T - T_prev).count();
+        curr_vel.x = (received_pos.x - prev_pos.x) / dT;
+        curr_vel.y = (received_pos.y - prev_pos.y) / dT;
+
+        // filtering velocity
+        double alpha_derivative = get_alpha(dT, derivative_cutoff);
+        curr_vel.x = solve_low_pass_filter(curr_vel.x, prev_vel.x, alpha_derivative);
+        curr_vel.y = solve_low_pass_filter(curr_vel.y, prev_vel.y, alpha_derivative);
+
+        // adapted alpha
+        double cutoff_frequency_x = min_cutoff_frequency + beta * abs(curr_vel.x);
+        double cutoff_frequency_y = min_cutoff_frequency + beta * abs(curr_vel.y);
+        double alpha_x = get_alpha(dT, cutoff_frequency_x);
+        double alpha_y = get_alpha(dT, cutoff_frequency_y);
+
+        // filtering
+        curr_pos.x = solve_low_pass_filter(received_pos.x, prev_pos.x, alpha_x);
+        curr_pos.y = solve_low_pass_filter(received_pos.y, prev_pos.y, alpha_y);
+
+        // ready for next
+        T_prev = T;
+        curr_acc = curr_vel - prev_vel;
+        prev_vel = curr_vel;
+        prev_pos = curr_pos;
+    }
 };
 
+// struct
 struct chaser_State
 {
     sf::CircleShape chaser_shape;
@@ -49,22 +122,13 @@ void chasers_initialize(std::vector<chaser_State>& chasers, sf::Vector2u window_
     }
 }
 
-void chasing(chaser_State& chaser, const sf::Vector2f& target_pos, sf::Vector2u window_size, float chasing_speed = 2e-5)
+void chasing(chaser_State& chaser, const sf::Vector2f& target_pos, sf::Vector2u window_size, float chasing_speed = 5e-5)
 {
-    float v = sqrt(pow(target_pos.x - chaser.position.x,2) + pow(target_pos.y - chaser.position.y,2));
+    float v = sqrt(float_square(target_pos.x - chaser.position.x) + float_square(target_pos.y - chaser.position.y));
     chaser.position.x += (target_pos.x - chaser.position.x)/v * chasing_speed;
     chaser.position.y += (target_pos.y - chaser.position.y)/v * chasing_speed;
     chaser.chaser_shape.setPosition({window_size.x * (chaser.position.x), window_size.y * (chaser.position.y)});
 }
-
-sf::Vector2f predict_Euler(player_State& state)
-{
-    // x_t+1 = xt + vt*t + 0.5*a*t^2
-    return {0.f, 0.f};
-}
-
-// constant
-const int socket_port_num = 5005;
 
 int main()
 {
@@ -93,7 +157,8 @@ int main()
 
 
     // player position 初期化
-    player_State player_state;
+    playerState player_state;
+    sf::Vector2f received_pos;
 
 
     while (window.isOpen())
@@ -111,17 +176,25 @@ int main()
             }
         }
         
-        
+
         // Socket通信
-        if (socket.receive(&player_state.curr_pos, sizeof(player_state.curr_pos), received, sender, port) == sf::Socket::Status::Done)
+        if (socket.receive(&received_pos, sizeof(received_pos), received, sender, port) == sf::Socket::Status::Done)
         {
-            player.setPosition({player_state.curr_pos.x * window_size.x, player_state.curr_pos.y * window_size.y});
+            player.setPosition({received_pos.x * window_size.x, received_pos.y * window_size.y});
+            // Player State Update
+            player_state.update(received_pos);
         }
+
 
         // 敵の追跡アルゴリズム
         
         // 単純追跡
-        for (auto& chaser : chasers) chasing(chaser, player_state.curr_pos, window_size);
+        // for (auto& chaser : chasers) chasing(chaser, player_state.get_curr_pos(), window_size);
+        for (int i = 0; i < 4; i++)
+        {
+            if (i < 2) chasing(chasers[i], player_state.get_curr_pos(), window_size);
+            else chasing(chasers[i], player_state.get_Euler_predict(), window_size);
+        }
 
 
         
@@ -130,7 +203,7 @@ int main()
         // 敵への当たり判定
         for (const auto& chaser : chasers)
         {
-            if (getDistance_square(player_state.curr_pos, chaser.position, window_size) < ((player_size_rad + chaser_size_rad) * (player_size_rad + chaser_size_rad)))
+            if (getDistance_square(player_state.get_curr_pos(), chaser.position, window_size) < ((player_size_rad + chaser_size_rad) * (player_size_rad + chaser_size_rad)))
             {
                 return 0;
             }
