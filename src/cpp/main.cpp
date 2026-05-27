779d7ef00036f3b2464ef0cb876f505e7025a1d9
dngmin <kimdongmin2248@gmail.com>
2026-05-28 01:15:43 +0900

    refactor: define new class GraphicsEngine

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -61,6 +61,70 @@ void red_message(std::string msg)
     std::cout << "\033[31m" << msg << "\033[0m" << std::endl;
 }
 
+class GraphicsEngine
+{
+public:
+    GraphicsEngine() :
+        window(sf::VideoMode(window_size), window_title),
+        player(player_size_rad),
+        chasers(4)
+        {
+            initPlayer();
+            initChaser();
+        };
+
+    // windw
+    static constexpr const char* window_title = "Escape from Finger chaser";
+    sf::Vector2u window_size = {800, 600};
+    sf::RenderWindow window;
+
+    // Object
+    static constexpr float player_size_rad = 10.f;
+    static constexpr float chaser_size_rad = 10.f;
+
+    sf::CircleShape player;
+    std::vector<chaser_State> chasers;
+
+    void UpdateWindowEvent()
+    {
+        while (std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>()) window.close();
+
+            else if (const auto* resized = event->getIf<sf::Event::Resized>())
+            {
+                window_size = window.getSize();
+                sf::View view(sf::FloatRect({0.f, 0.f}, sf::Vector2f(window_size)));
+                window.setView(view);
+            }
+        }
+    }
+
+private:
+    void initPlayer()
+    {
+        sf::Vector2f init_pos = {0.5, 0.5};
+        player.setOrigin({player_size_rad, player_size_rad});
+        player.setPosition({init_pos.x * window_size.x, init_pos.y * window_size.y});
+    };
+
+    void initChaser()
+    {
+    std::vector<sf::Vector2f> init_pos = {{0.1, 0.1}, {0.9, 0.1}, {0.1, 0.9}, {0.9, 0.9}};
+    std::vector<std::vector<uint8_t>> init_color = {{255, 0, 0}, {0, 255, 0}, {0, 255, 255}, {255, 255, 0}};
+    for (int i = 0; i < 4; i++)
+    {
+        chasers[i].position = init_pos[i];
+        init_pos[i].x *= window_size.x;
+        init_pos[i].y *= window_size.y;
+        chasers[i].chaser_shape.setRadius(chaser_size_rad);
+        chasers[i].chaser_shape.setOrigin({chaser_size_rad, chaser_size_rad});
+        chasers[i].chaser_shape.setPosition(init_pos[i]);
+        chasers[i].chaser_shape.setFillColor(sf::Color(init_color[i][0], init_color[i][1], init_color[i][2]));
+    }
+    };
+};
+
 int main()
 {
     try
@@ -69,44 +133,16 @@ int main()
         SocketSetting socket_setting;
         socket_setting.init();
         
-
         // SFML 初期化
-        sf::Vector2u window_size = {800, 600};
-        sf::RenderWindow window(sf::VideoMode(window_size), "display");
-
-        float player_size_rad = 10.f;
-        float chaser_size_rad = 10.f;
-
-        // player
-        sf::CircleShape player(player_size_rad);
-        player.setOrigin({player_size_rad, player_size_rad});
-        // 敵
-        std::vector<chaser_State> chasers(4);
-        chasers_initialize(chasers, window_size, chaser_size_rad);
+        GraphicsEngine graphics_engine;
 
         // player position 初期化
         PlayerState player_state;
-        sf::Vector2f received_pos;
 
-        {
-            sf::Vector2f curr_pos = player_state.get_curr_pos();
-            player.setPosition({curr_pos.x * window_size.x, curr_pos.y * window_size.y});
-        }
-
-        while (window.isOpen())
+        while (graphics_engine.window.isOpen())
         {
             // SFML evnet
-            while (std::optional event = window.pollEvent())
-            {
-                if (event->is<sf::Event::Closed>()) window.close();
-
-                else if (const auto* resized = event->getIf<sf::Event::Resized>())
-                {
-                    window_size = window.getSize();
-                    sf::View view(sf::FloatRect({0.f, 0.f}, sf::Vector2f(window_size)));
-                    window.setView(view);
-                }
-            }
+            graphics_engine.UpdateWindowEvent();
             
             // Socket通信
             if (socket_setting.receive())
@@ -118,7 +154,7 @@ int main()
 
                 // set player
                 sf::Vector2f curr_pos = player_state.get_curr_pos();
-                player.setPosition({curr_pos.x * window_size.x, curr_pos.y * window_size.y});
+                graphics_engine.player.setPosition({curr_pos.x * graphics_engine.window_size.x, curr_pos.y * graphics_engine.window_size.y});
             }
 
             // 敵の追跡アルゴリズム
@@ -127,21 +163,21 @@ int main()
             // for (auto& chaser : chasers) chasing(chaser, player_state.get_curr_pos(), window_size);
             for (int i = 0; i < 4; i++)
             {
-                if (i < 2) chasing(chasers[i], player_state.get_curr_pos(), window_size);
-                else chasing(chasers[i], player_state.get_Euler_predict(), window_size);
+                if (i < 2) chasing(graphics_engine.chasers[i], player_state.get_curr_pos(), graphics_engine.window_size);
+                else chasing(graphics_engine.chasers[i], player_state.get_Euler_predict(), graphics_engine.window_size);
             }
 
             // 描画
-            window.clear(sf::Color::Black);
-            window.draw(player);
-            for (auto& chaser : chasers) window.draw(chaser.chaser_shape);
+            graphics_engine.window.clear(sf::Color::Black);
+            graphics_engine.window.draw(graphics_engine.player);
+            for (auto& chaser : graphics_engine.chasers) graphics_engine.window.draw(chaser.chaser_shape);
 
-            window.display();
+            graphics_engine.window.display();
 
             // 敵への当たり判定
-            for (const auto& chaser : chasers)
+            for (const auto& chaser : graphics_engine.chasers)
             {
-                if (getDistance_square(player_state.get_curr_pos(), chaser.position, window_size) < ((player_size_rad + chaser_size_rad) * (player_size_rad + chaser_size_rad)))
+                if (getDistance_square(player_state.get_curr_pos(), chaser.position, graphics_engine.window_size) < ((graphics_engine.player_size_rad + graphics_engine.chaser_size_rad) * (graphics_engine.player_size_rad + graphics_engine.chaser_size_rad)))
                 {
                     return 0;
                 }
