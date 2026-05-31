32f6d4a07354bcacedf79497bd5eefcc8fb3d4d6
dngmin <kimdongmin2248@gmail.com>
2026-05-31 17:33:01 +0900

    refactor: introduce GameEngine and merge chaser_configs into chasers_config GameEngine now handles all initialization. Merge chaser1_config~chaser4_config into chasers_config and add comments.

--- include/GameConfig.hpp
+++ include/GameConfig.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <vector>
 
+// ゲーム開始に必要な全ての初期値を管理している
+// 0~1の比率で正規化した座標系を使用するため初期位置も0~1の値にする
 struct GameConfig
 {
     struct WindowConfig
@@ -17,8 +20,16 @@ struct GameConfig
         sf::Color color = sf::Color::White;
     };
 
+    struct ChasersConfig
+    {
+        size_t chaserCount = 0;
+        std::vector<EntityConfig> chasers;
+    };
+
     WindowConfig window_config;
+
     EntityConfig player_config;
+
     EntityConfig chaser1_config = {
         .init_pos = {0.1f, 0.1f},
         .color = sf::Color::Red
@@ -34,5 +45,15 @@ struct GameConfig
     EntityConfig chaser4_config = {
         .init_pos = {0.9f, 0.9f},
         .color = sf::Color::Green
+    };
+
+    ChasersConfig chasers_config = {
+        .chaserCount = 4,
+        .chasers = {
+            chaser1_config,
+            chaser2_config,
+            chaser3_config,
+            chaser4_config
+        }
     };
 };
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
 #include <string>
-#include <stdexcept>
 
 #include "Utils.hpp"
 
@@ -16,14 +14,35 @@
 // グラフィックスUI
 #include "GraphicsEngine.hpp"
 
-// SFML
-#include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-
 void red_message(std::string msg)
 {
     std::cout << "\033[31m" << msg << "\033[0m" << std::endl;
 }
+
+class GameEngine
+{
+public:
+    GameEngine(const GameConfig& config)
+        : graphics_engine(config.window_config)
+        , player(config.player_config)
+    {
+        // 初回描画のために、正規化された座標の変換処理を1回だけ行う
+        player.update(config.player_config.init_pos, config.window_config.window_size);
+
+        chasers.reserve(config.chasers_config.chaserCount);
+        for (auto& chaser_config : config.chasers_config.chasers)
+        {
+            chasers.emplace_back(chaser_config);
+        }
+    }
+
+    SocketHandler socket_handler;
+    GraphicsEngine graphics_engine;
+    Player player;
+    std::vector<Chaser> chasers;
+
+private:
+};
 
 int main()
 {
@@ -31,57 +50,42 @@ int main()
     {
         // ゲーム設定
         GameConfig game_config;
+        GameEngine game_engine(game_config);
 
-        // Udp Socket 初期化
-        SocketHandler socket_handler;
-
-        // SFML 初期化
-        GraphicsEngine graphics_engine(game_config.window_config);
-
-        // オブジェクト初期化
-        static constexpr float playerSize = 10.f, chaserSize = 10.f;
-        Player player(game_config.player_config);
-
-        Chaser chaser1(game_config.chaser1_config);
-        Chaser chaser2(game_config.chaser2_config);
-        Chaser chaser3(game_config.chaser3_config);
-        Chaser chaser4(game_config.chaser4_config);
-
-
-
-        while (graphics_engine.window.isOpen())
+        while (game_engine.graphics_engine.window.isOpen())
         {
             // SFML evnet
-            graphics_engine.UpdateWindowEvent();
+            game_engine.graphics_engine.UpdateWindowEvent();
             
             // Socket通信
-            if (socket_handler.receive())
+            if (game_engine.socket_handler.receive())
             {
-                sf::Vector2f received_pos = socket_handler.get_received_pos();
+                sf::Vector2f received_pos = game_engine.socket_handler.get_received_pos();
 
                 // Player State Update
-                player.update(received_pos, graphics_engine.window_size);
+                game_engine.player.update(received_pos, game_engine.graphics_engine.window_size);
             }
 
             // 敵の追跡アルゴリズム
-            chaser1.update(player.getPosition(), graphics_engine.window_size);
-            chaser2.update(player.getPosition(), graphics_engine.window_size);
-            chaser3.update(player.getEulerPredict(), graphics_engine.window_size);
-            chaser4.update(player.getEulerPredict(), graphics_engine.window_size);
+            for (int i = 0; i < game_config.chasers_config.chaserCount; i++)
+            {
+                if (i < 2) game_engine.chasers[i].update(game_engine.player.getPosition(), game_engine.graphics_engine.window_size);
+                else game_engine.chasers[i].update(game_engine.player.getEulerPredict(), game_engine.graphics_engine.window_size);
+            }
 
             // 描画
-            graphics_engine.Render
+            game_engine.graphics_engine.Render
             (
-                player.getEntity(),
-                {chaser1.getEntity()
-                ,chaser2.getEntity()
-                ,chaser3.getEntity()
-                ,chaser4.getEntity()});
+                game_engine.player.getEntity(),
+                {game_engine.chasers[0].getEntity()
+                ,game_engine.chasers[1].getEntity()
+                ,game_engine.chasers[2].getEntity()
+                ,game_engine.chasers[3].getEntity()});
 
             // 敵への当たり判定
-            for (const auto& chaser : {chaser1, chaser2, chaser3, chaser4})
+            for (int i = 0; i < game_config.chasers_config.chaserCount; i++)
             {
-                if (getDistance(player.getPosition(), chaser.getPosition(), graphics_engine.window_size) < (playerSize + chaserSize))
+                if (getDistance(game_engine.player.getPosition(), game_engine.chasers[i].getPosition(), game_engine.graphics_engine.window_size) < (game_config.player_config.entity_size + game_config.chasers_config.chasers[i].entity_size))
                 {
                     return 0;
                 }
